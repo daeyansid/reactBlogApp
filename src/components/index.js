6b70302634177b9e6d2220a1ca2df93042ca321a
Daeyan_server <daeyansidi826@gmail.com>
2024-06-09 01:01:49 +0500

    2 librairs added hook form and router dom + som components made and authLayout aka session handling is set

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,19 @@ import Footer from "./footer/footer"
 import Container from "./container/container"
 import Logo from "./logo"
 import logoutBtn from "./header/logout"
+import Button from "./button"
+import PostCard from "./postCard"
+import Input from "./input"
+import select from "./select"
 
 export {
     Header,
     Footer,
     Logo,
     Container,
-    logoutBtn
+    logoutBtn,
+    Button,
+    Input,
+    select,
+    PostCard
 }
