1f27a237f525dc84b8e424ac9891092e1bf8046b
Daeyan_server <daeyansidi826@gmail.com>
2024-06-12 02:36:10 +0500

    pages added1

---
+++ src/pages/signUp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import signUp from "../components/index";
+export default function signUp() {
+    return(
+        <div className="py-8">
+            <signUp />
+        </div>
+    )
+}
