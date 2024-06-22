73818b46dd9487ec936cbf22e48bdb104ea1169c
Daeyan_server <daeyansidi826@gmail.com>
2024-06-22 14:21:10 +0500

    login and register works

--- src/components/index.js
+++ src/components/index.js
@@ -1,24 +1,24 @@
-import Header from "./header/header"
-import Footer from "./footer/footer"
-import Container from "./container/container"
-import Logo from "./logo"
-import logoutBtn from "./header/logout"
-import Button from "./button"
-import PostCard from "./postCard"
-import Input from "./input"
-import select from "./select"
-import RTE from "./rte"
-import postForm from "./post-form/postForm"
-import SignUp from "./signUp"
-import Login from "./login"
-import AuthLayout from './authLayout'
+import Header from "./header/header";
+import Footer from "./footer/footer";
+import Container from "./container/container";
+import Logo from "./logo";
+import LogoutBtn from "./header/logout";
+import Button from "./button";
+import PostCard from "./postCard";
+import Input from "./input";
+import select from "./select";
+import RTE from "./rte";
+import postForm from "./post-form/postForm";
+import SignUp from "./signUp";
+import Login from "./login";
+import AuthLayout from './authLayout';
 
 export {
     Header,
     Footer,
     Logo,
     Container,
-    logoutBtn,
+    LogoutBtn,
     Button,
     Input,
     select,
@@ -28,4 +28,4 @@ export {
     SignUp,
     Login,
     AuthLayout,
-}
+};
